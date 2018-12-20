41ba0c597e82ef86ac800c7fd444dee1a65329de
talifa <talifaidk@gmail.com>
2018-12-20 23:29:59 +0300

    tttt

---
+++ lazy/js/common.js
@@ -0,0 +1,10 @@
+$(function() {
+
+const observer = lozad(); // lazy loads elements with default selector as '.lozad'
+observer.observe();
+
+});
+document.addEventListener("DOMContentLoaded", yall);
+var bLazy = new Blazy({
+  
+});
